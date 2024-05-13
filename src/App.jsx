48f51bba4a89995ec32bd2521776bd3833288d3e
Gyeoul <clsmay.1st@gmail.com>
2024-05-13 20:55:21 +0900

    [TASK] Make Header Component
    Tasks:
    * Home Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const rendering = () => {
+    const result = [];
+    for (let i = 0; i < 200; i++) {
+      result.push(<span key={i}>{i}</span>);
+    }
+    return result;
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+        <Header/>
+        <main style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#fff",
+          minHeight: "calc(100vh - 100px)",
+          padding: "20px"
+        }}>
+          <h1>Hello World</h1>
+          {rendering()}
+        </main>
+        <Footer/>
+      </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/foo",
+    element: <Footer/>
+  },
+  {
+    path: "/head",
+    element: <Header/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      {/*<App />*/}
+      <RouterProvider router={router}/>
+    </React.StrictMode>,
 )
