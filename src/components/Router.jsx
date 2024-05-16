700bfc26e378937936aa9476da03c05e8905d76a
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-05-16 14:26:26 +0900

    [TASK] Login, Registration, Community page (#4)
    * ComfirmDeleteModal
    
    * [TASK] Login Page
    
    * [TASK] auth page, Comunity page
    
    ---------
    
    Co-authored-by: Gyeoul <clsmay.1st@gmail.com>

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,17 +1,37 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Footer from './Footer.jsx';
-import Header from './headerComponents/Header.jsx';
-import HomePage from './page/HomePage.jsx';
+import Header from './Header.jsx';
 import Login from '../pages/auth/Login.jsx';
 import Registration from '../pages/auth/Registration.jsx';
 import FindAccount from '../pages/auth/FindAccount.jsx';
 import FindPassword from '../pages/auth/FindPassword.jsx';
 import Community from '../pages/Community.jsx';
+import HomePage from './page/HomePage.jsx';
 
 const Router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/foo',
+    element: <Footer />,
+  },
+  {
+    path: '/head',
+    element: <Header />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/registration',
+    element: <Registration />,
+  },
+  {
+    path: '/findAccount',
+    element: <FindAccount />,
   },
   {
     path: '/foo',
