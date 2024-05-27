6778de4dc5ebcc3130f535e6e3e89ff0f2042be0
ssunnykku <ssunnykku@gmail.com>
2024-05-27 11:16:44 +0900

    .env 적용

--- src/apis/Community.jsx
+++ src/apis/Community.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
+const url = process.env.VITE_API_URL;
+
 export const axiosCommunityDetail = async (params) => {
-  return await axios.get(
-    `http://127.0.0.1:8080/api/posts/imageList?postId=${params['id']}`
-  );
+  return await axios.get(url + `/posts/imageList?postId=${params['id']}`);
 };
 
 // export const axiosAddCommunity = async (params) => {
@@ -18,47 +18,42 @@ export const axiosCommunityDetail = async (params) => {
 
 export const axiosCommunity = async (params) => {
   return await axios.get(
-    `http://127.0.0.1:8080/api/communities/detail?communityId=${params['id']}`
+    url + `/communities/detail?communityId=${params['id']}`
   );
 };
 
 export const axiosMyCommunityList = async (params) => {
-  return await axios.get(
-    `http://localhost:8080/api/myCommunity?userId=${params['userId']}`
-  );
+  return await axios.get(url + `/myCommunity?userId=${params['userId']}`);
 };
 
 export const axiosAllCommunityList = async () => {
-  return await axios.get('http://localhost:8080/api/communities/allCommunity');
+  return await axios.get(url + '/communities/allCommunity');
 };
 
 export const axiosMemberCommunityPostList = async (params) => {
-  return await axios.get(
-    'http://localhost:8080/api/communityPosts/memberPage',
-    { params }
-  );
+  return await axios.get(url + '/communityPosts/memberPage', { params });
 };
 
 export const axiosGuestCommunityPostList = async (params) => {
-  return await axios.get('http://localhost:8080/api/communityPosts/guest', {
+  return await axios.get(url + `/communityPosts/guest`, {
     params,
   });
 };
 
 export const axiosCurrentMemberList = async (params) => {
   return await axios.get(
-    `http://localhost:8080/api/communities/currentMember?communityId=${params['id']}`
+    url + `/communities/currentMember?communityId=${params['id']}`
   );
 };
 
 export const axiosSearchNickname = async (params) => {
   return await axios.get(
-    `http://localhost:8080/api/searchNickname?nickname=${params['nickname']}`
+    url + `/searchNickname?nickname=${params['nickname']}`
   );
 };
 
 export const axiosAddMember = async (communityId, userId) => {
   return await axios.post(
-    `http://localhost:8080/api/communities/member/add?communityId=${communityId}&userId=${userId}`
+    url + `/communities/member/add?communityId=${communityId}&userId=${userId}`
   );
 };
--- src/apis/Feed.jsx
+++ src/apis/Feed.jsx
@@ -1,25 +1,23 @@
 import axios from 'axios';
 
+const url = process.env.VITE_API_URL;
+
 export const postImageList = async (params) => {
-  return await axios.get(
-    `http://127.0.0.1:8080/api/posts/imageList?postId=${params['id']}`
-  );
+  return await axios.get(url + `/posts/imageList?postId=${params['id']}`);
 };
 
 export const axiosFeed = async (params) => {
-  return await axios.get(
-    `http://127.0.0.1:8080/api/posts/feed?postId=${params['id']}`
-  );
+  return await axios.get(url + `/posts/feed?postId=${params['id']}`);
 };
 
 export const axiosHomeFeed = async () => {
-  return await axios.get('http://127.0.0.1:8080/api/posts');
+  return await axios.get(url + '/posts');
 };
 
 export const axiosAddFeed = async (params) => {
   return await axios.post(
     // `http://localhost:8080/api/posts/create?postTitle=${params['postTitle']}&postDescription=${params['postDescription']}&postHashtag=${params['postHashtag']}&postStatus=${parmas['postStatus']}&userId=${params['userId']}`
-    'http://localhost:8080/api/posts/create',
+    url + '/posts/create',
     null,
     {
       params: params,
